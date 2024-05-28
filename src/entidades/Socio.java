28428ccae872e548f29cb455bae0d5f065dd061a
Martin Piriz <mpirizdutra@ulp.edu.ar>
2024-05-28 19:01:15 -0300

    socio

--- src/entidades/Socio.java
+++ src/entidades/Socio.java
@@ -5,5 +5,99 @@ package entidades;
  * @author Nicolas
  */
 public class Socio {
+    //`idSocio`, `dni`, `nombre`, `apellido`, `edad`, `correo`, `telefono` FROM `socios`
+    private int idSocio;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String correo;
+    private String telefono;
+
+    public Socio() {
+    }
+
+    public Socio(int idSocio, String dni, String nombre, String apellido, int edad, String correo, String telefono) {
+        this.idSocio = idSocio;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public Socio(String dni, String nombre, String apellido, int edad, String correo, String telefono) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return dni+", "+ nombre+", "+apellido; // es para los comboBox
+    }
+
+    public int getIdSocio() {
+        return idSocio;
+    }
+
+    public void setIdSocio(int idSocio) {
+        this.idSocio = idSocio;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
     
 }
