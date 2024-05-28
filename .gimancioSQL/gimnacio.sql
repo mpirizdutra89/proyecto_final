0d978b377e2f86b8e62eaab2975f8f5dc68eff09
Martin Piriz <mpirizdutra@ulp.edu.ar>
2024-05-28 18:21:26 -0300

    cambios

--- .gimancioSQL/gimnacio.sql
+++ .gimancioSQL/gimnacio.sql
@@ -1,21 +1,11 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 28-05-2024 a las 17:58:54
--- Versión del servidor: 8.2.0
--- Versión de PHP: 8.2.13
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+
 
 --
 -- Base de datos: `gimnacio`
@@ -56,6 +46,22 @@ CREATE TABLE IF NOT EXISTS `entrenadores` (
   `disponibilidad` varchar(80) COLLATE utf8mb4_general_ci NOT NULL COMMENT 'El horiario del profesor',
   PRIMARY KEY (`idEntrenadore`),
   UNIQUE KEY `dni_UNIQUE` (`dni`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inscribirseclase`
+--
+
+DROP TABLE IF EXISTS `inscribirseclase`;
+CREATE TABLE IF NOT EXISTS `inscribirseclase` (
+  `clases_idclases` int NOT NULL,
+  `socios_idSocio` int NOT NULL,
+  `fecha` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`clases_idclases`,`socios_idSocio`),
+  KEY `fk_clases_has_socios_socios1_idx` (`socios_idSocio`),
+  KEY `fk_clases_has_socios_clases1_idx` (`clases_idclases`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -105,12 +111,17 @@ ALTER TABLE `clases`
   ADD CONSTRAINT `fk_clases_entrenadores1` FOREIGN KEY (`idEntrenadore`) REFERENCES `entrenadores` (`idEntrenadore`);
 
 --
+-- Filtros para la tabla `inscribirseclase`
+--
+ALTER TABLE `inscribirseclase`
+  ADD CONSTRAINT `fk_clases_has_socios_clases1` FOREIGN KEY (`clases_idclases`) REFERENCES `clases` (`idclases`),
+  ADD CONSTRAINT `fk_clases_has_socios_socios1` FOREIGN KEY (`socios_idSocio`) REFERENCES `socios` (`idSocio`);
+
+--
 -- Filtros para la tabla `membresias`
 --
 ALTER TABLE `membresias`
   ADD CONSTRAINT `fk_membresias_socios1` FOREIGN KEY (`idSocio`) REFERENCES `socios` (`idSocio`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
