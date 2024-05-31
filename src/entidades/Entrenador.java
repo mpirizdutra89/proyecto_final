175332360d7cdb65ba32c675fe5c03cb942a8cce
mpirizdutra89 <mpirizdutra@ulp.edu.ar>
2024-05-31 18:04:47 -0300

    Update Entrenador.java

--- src/entidades/Entrenador.java
+++ src/entidades/Entrenador.java
@@ -2,8 +2,89 @@ package entidades;
 
 /**
  *
- * @author Nicolas
+ * @author agus1
  */
 public class Entrenador {
+    private int idEntrenador;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private String especialidad;
+    private boolean estado;
+
+    public Entrenador() {
+    }
+    
+    public Entrenador(String dni, String nombre, String apellido, String especialidad, boolean estado) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.especialidad = especialidad;
+        this.estado = estado;
+    }
+    
+    public Entrenador(int idEntrenador,String dni,String nombre, String apellido, String especialidad, boolean estado){
+        this.idEntrenador = idEntrenador;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.especialidad = especialidad;
+        this.estado = estado;
+    }
+
+    public int getIdEntrenador() {
+        return idEntrenador;
+    }
+
+    public void setIdEntrenador(int idEntrenador) {
+        this.idEntrenador = idEntrenador;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+        @Override
+    public String toString() {
+        return nombre+", "+apellido+", "+idEntrenador+", "+especialidad;
+    }
+    
+    
     
 }
