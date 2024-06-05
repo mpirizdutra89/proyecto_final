6215ae3fddaf9ba2aa2c6dad8bcd6bf5a395e96c
Martin Piriz <mpirizdutra@ulp.edu.ar>
2024-06-05 18:43:18 -0300

    Update main.java

--- src/Test/main.java
+++ src/Test/main.java
@@ -1,5 +1,8 @@
 package Test;
 
+import entidades.Socio;
+import java.util.ArrayList;
+
 /**
  *
  * @author Nicolas
@@ -18,11 +21,15 @@ public class main {
 
         accesoADatos.SocioData socioDat=new accesoADatos.SocioData();
         //guardarSocio
-        socioDat.guardarSocio(new entidades.Socio("34877112","martin","piriz",32,"mpirizdutra@ulp.edu.ar","2664859913"));
-        socioDat.guardarSocio(new entidades.Socio("18762864","Walter","piriz",32,"wepo56@gmail.com","2664855569"));
-        socioDat.guardarSocio(new entidades.Socio("52909515","Silvia","Dutra",32,"dialviacaetano63@gmail.com","2664761074"));
+        socioDat.guardarSocio(new Socio("34877112","martin","piriz",32,"mpirizdutra@ulp.edu.ar","2664859913"));
+        socioDat.guardarSocio(new Socio("18762864","Walter","piriz",32,"wepo56@gmail.com","2664855569"));
+        socioDat.guardarSocio(new Socio("52909515","Silvia","Dutra",32,"dialviacaetano63@gmail.com","2664761074"));
         
-        
+        ArrayList<Socio> lista=new ArrayList<Socio>();
+        lista=socioDat.listarSocio();
+        for (Socio socio : lista) {
+            System.out.println(socio);
+        }
        
         
         
@@ -30,5 +37,8 @@ public class main {
         
         
     }
+    
+    
+    
     
 }
