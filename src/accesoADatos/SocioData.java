3cb03ebad5fd3356a3e04ac9d7f95ea0439a8700
mpirizdutra89 <mpirizdutra@ulp.edu.ar>
2024-06-05 11:49:18 -0300

    guardarSocio

--- src/accesoADatos/SocioData.java
+++ src/accesoADatos/SocioData.java
@@ -1,9 +1,60 @@
 package accesoADatos;
 
+import entidades.Socio;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 /**
  *
- * @author Martin
+ * @author Martin Piriz
  */
 public class SocioData {
+    
+    private Socio socio;
+    private static Connection conec = null;
+
+    public SocioData() {
+        conec=null;
+        conec = Conexion.getConexion();
+    }
+    
+    //SELECT `idSocio`, `dni`, `nombre`, `apellido`, `edad`, `correo`, `telefono`, `estado` FROM `socios`
+    public boolean guardarSocio(Socio socio) {
+        String query = "INSERT INTO socios (dni,nombre,apellido,edad,correo,telefono) VALUES (?,?,?,?,?,?)";
+
+        boolean res = false;
+
+        try {
+            PreparedStatement ps = conec.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, socio.getDni());
+            ps.setString(2, socio.getNombre());
+            ps.setString(3, socio.getApellido());
+            ps.setInt(4,socio.getEdad());
+            ps.setString(5, socio.getCorreo());
+            ps.setString(6, socio.getTelefono());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                socio.setIdSocio(rs.getInt(1));
+                
+                res = true;
+            }
+            ps.close();
+            rs.close();
+        } catch (SQLException  | NullPointerException ex) {
+            Conexion.msjError.add("Socio: guardarSocio() ->" + ex.getMessage());
+        }
+
+       
+        return res;
+    }
+    
+    
+    
+    
+    
     
 }
