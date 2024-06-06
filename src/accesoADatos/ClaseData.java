dc9ef55a9a31a3bd7fbe7a66b35b7ac2b8d5e2ad
mpirizdutra89 <mpirizdutra@ulp.edu.ar>
2024-06-06 15:15:42 -0300

    Update ClaseData.java

--- src/accesoADatos/ClaseData.java
+++ src/accesoADatos/ClaseData.java
@@ -4,8 +4,7 @@ import entidades.Clase;
 import java.sql.*;
 import java.time.*;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 import javax.swing.JOptionPane;
 
 /**
